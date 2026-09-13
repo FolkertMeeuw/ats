42736ceff389fc21c06d16e33dfe87668a9ff7e0
Folkert Meeuw <folkert.meeuw@googlemail.com>
2026-09-13 16:14:27 +0200

    Remove redundant init_ollama.sh script

--- scripts/init_ollama.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if ! pgrep -f "ollama serve" > /dev/null; then
-    ollama serve > /dev/null 2>&1 &
-    sleep 3
-fi
-#ollama pull llama3.2
-#ollama pull llama3.2:1b
-#ollama pull llama3.2-vision
-ollama pull moondream
